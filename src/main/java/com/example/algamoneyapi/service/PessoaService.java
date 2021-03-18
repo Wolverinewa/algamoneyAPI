1981b1ffb7bade7557fcd1fcf1f71f7c91eee256
Willian <willian.adao@linkc.com.br>
2021-03-18 10:15:05 -0300

    Finalizado 4-Atualização e remoção de recursos na API

---
+++ src/main/java/com/example/algamoneyapi/service/PessoaService.java
@@ -0,0 +1,42 @@
+package com.example.algamoneyapi.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.example.algamoneyapi.model.Pessoa;
+import com.example.algamoneyapi.repository.PessoaRepository;
+
+@Service
+public class PessoaService {
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	public Pessoa atualizar(@PathVariable Long codigo, @RequestBody Pessoa pessoa) {
+
+		  Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
+
+		  BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+
+		  return this.pessoaRepository.save(pessoaSalva);
+	}
+
+	private Pessoa buscarPessoaPeloCodigo(Long codigo) {
+		Pessoa pessoaSalva = this.pessoaRepository.findById(codigo)
+		      .orElseThrow(() -> new EmptyResultDataAccessException(1));
+		return pessoaSalva;
+	}
+
+	public void atualizarPropriedadeAtivo(Long codigo, Boolean ativo) {
+
+		Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
+		pessoaSalva.setAtivo(ativo);
+		pessoaRepository.save(pessoaSalva);
+		
+	}
+
+}
